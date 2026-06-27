2e4e13e2f87f036f046465a50d5e5a2177aaf32f
Ayush <saha92873@gmail.com>
2026-06-27 22:14:18 +0530

    updated week 1 work

---
+++ Week1/JUnit_Basic Testing excercise/FirstJUnitTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class FirstJUnitTest {
+    
+    @Test
+    public void testEnvironmentSetup() {
+
+        assertTrue("JUnit is successfully set up!", true);
+    }
+}
